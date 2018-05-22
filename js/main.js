aa02d0761414583eaedc0f2bfc7d236af8dc78af
david milivojev <davyd7_@hotmail.com>
2018-05-23 01:36:14 +0200

    add responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var scroll = document.getElementById('up');
 
+$(document).ready(function(){
+  //hamburger
+  $('.nav-toggle').click(function(){
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+  })
+})
 scroll.addEventListener('click', function(){
   window.scrollTo(0,0);
 });
